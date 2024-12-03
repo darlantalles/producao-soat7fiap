632b188d895d666d42917bfc02402135fc3802db
Darlan Talles <darlantalles@gmail.com>
2024-12-03 19:04:03 -0300

    Inclusão api de cadastro de cliente

---
+++ src/main/java/br/com/fiap/soat7/application/usecases/cliente/AdicionarClienteUsecase.java
@@ -0,0 +1,18 @@
+package br.com.fiap.soat7.application.usecases.cliente;
+
+import br.com.fiap.soat7.application.gateways.ClienteGateway;
+import br.com.fiap.soat7.domain.Cliente;
+public class AdicionarClienteUsecase {
+
+    private final ClienteGateway clienteGateway;
+
+
+    public AdicionarClienteUsecase(ClienteGateway clienteGateway) {
+        this.clienteGateway = clienteGateway;
+    }
+
+
+    public Cliente criarCliente(Cliente cliente) {
+        return clienteGateway.createCliente(cliente);
+    }
+}
---
+++ src/main/java/br/com/fiap/soat7/application/usecases/cliente/BuscarTodosClientesUsecase.java
@@ -0,0 +1,18 @@
+package br.com.fiap.soat7.application.usecases.cliente;
+
+import br.com.fiap.soat7.application.gateways.ClienteGateway;
+import br.com.fiap.soat7.domain.Cliente;
+import java.util.List;
+
+public class BuscarTodosClientesUsecase {
+
+    private final ClienteGateway clienteGateway;
+
+    public BuscarTodosClientesUsecase(ClienteGateway clienteGateway) {
+        this.clienteGateway = clienteGateway;
+    }
+
+    public List<Cliente> buscarTodosClientes() {
+        return clienteGateway.findAll();
+    }
+}
